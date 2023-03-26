70f5340b12a4b66c5dd4da1575d00f058027b9c0
Pontus Doverstav <doverstav@gmail.com>
2023-03-26 11:15:51 +0200

    WIP project filtering

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { useCallback, useMemo, useState } from "react";
 import Layout from "../../../components/layout";
 import { getProjectsData, ProjectData } from "../../../utils/projects";
 
@@ -7,11 +8,49 @@ interface ProjectsProps {
 }
 
 export default function Projects({ allProjectsData }: ProjectsProps) {
+  const [selectedTags, setSelectedTags] = useState<string[]>([]);
+
+  const allTags = useMemo(() => {
+    const tagSet = new Set<string>();
+    allProjectsData.forEach((projectData) =>
+      projectData.tags.forEach((tag) => tagSet.add(tag))
+    );
+
+    return Array.from(tagSet);
+  }, [allProjectsData]);
+
+  const filteredProjects = useMemo(
+    () =>
+      selectedTags.length === 0
+        ? allProjectsData
+        : allProjectsData.filter((projectData) =>
+            projectData.tags.some((tag) => selectedTags.includes(tag))
+          ),
+    [selectedTags, allProjectsData]
+  );
+
+  const handleFilterButtonClick = (tagClicked: string) => {
+    setSelectedTags((prev) =>
+      prev.includes(tagClicked)
+        ? prev.filter((tag) => tag !== tagClicked)
+        : [...prev, tagClicked]
+    );
+  };
+
   return (
     <Layout>
       <h1>Currently {allProjectsData.length} projects:</h1>
+      <h2>Filter</h2>
       <div>
-        {allProjectsData.map((projectFrontmatter) => (
+        {allTags.map((tag) => (
+          <button key={tag} onClick={() => handleFilterButtonClick(tag)}>
+            {tag}
+          </button>
+        ))}
+      </div>
+      <hr />
+      <div>
+        {filteredProjects.map((projectFrontmatter) => (
           <>
             <Link href={`/projects/${projectFrontmatter.id}`}>
               {projectFrontmatter.title}
