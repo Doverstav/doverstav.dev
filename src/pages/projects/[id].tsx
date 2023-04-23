2fb71258b34e7c9a3b7c97c6ecf3d384097ffb8b
Pontus Doverstav <doverstav@gmail.com>
2023-04-23 11:57:43 +0200

    Use next/image for markdown images

--- src/pages/projects/[id].tsx
+++ src/pages/projects/[id].tsx
@@ -10,12 +10,19 @@ import { TagPill } from "../../../components/TagPill";
 import Link from "next/link";
 import styles from "./projects.module.css";
 import ReactMarkdown from "react-markdown";
+import sizeOf from "image-size";
+import { join } from "path";
+import Image from "next/image";
 
 interface ProjectPageProps {
   projectData: ProjectDataWithContent;
+  imageSizes: Record<string, { width: number; height: number }>;
 }
 
-export default function ProjectPage({ projectData }: ProjectPageProps) {
+export default function ProjectPage({
+  projectData,
+  imageSizes,
+}: ProjectPageProps) {
   return (
     <Layout>
       <Head>
@@ -35,6 +42,22 @@ export default function ProjectPage({ projectData }: ProjectPageProps) {
             link: ({ href, children }) => (
               <Link href={href ?? ""}>{children}</Link>
             ),
+            img: (props) => {
+              if (props.src && imageSizes[props.src]) {
+                const { src, alt } = props;
+                const { width, height } = imageSizes[props.src];
+                return (
+                  <Image
+                    src={src}
+                    alt={alt ?? ""}
+                    width={width}
+                    height={height}
+                  />
+                );
+              } else {
+                return <img {...props} />;
+              }
+            },
           }}
         >
           {projectData.content}
@@ -58,9 +81,26 @@ export function getStaticPaths() {
 export async function getStaticProps({ params }: Params) {
   const projectData = await getProjectData(params.id);
 
+  const imageSizes: ProjectPageProps["imageSizes"] = {};
+
+  // A regular expression to iterate on all images in the post
+  const iterator = projectData.content.matchAll(/\!\[.*]\((.*)\)/g);
+  let match: IteratorResult<RegExpMatchArray, any>;
+  while (!(match = iterator.next()).done) {
+    const [, src] = match.value;
+    try {
+      // Images are stored in `public`
+      const { width, height } = sizeOf(join("public", src));
+      imageSizes[src] = { width: width ?? 500, height: height ?? 500 };
+    } catch (err) {
+      console.error(`Can’t get dimensions for ${src}:`, err);
+    }
+  }
+
   return {
     props: {
       projectData,
+      imageSizes,
     },
   };
 }
